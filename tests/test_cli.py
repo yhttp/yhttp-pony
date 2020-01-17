75c87a44ed83703d38e62825026a11e1a9d2f4b8
pylover <vahid.mardani@gmail.com>
2020-01-17 13:42:34 +0330

    test db create and drop commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
-from bddcli import Given, Application as CLIApplication, status, stderr, stdout
+from bddcli import Given, Application as CLIApplication, status, stderr, \
+    stdout, when
 from yhttp import Application
 from yhttp.extensions.pony import setup as setuporm, configure as configureorm
 
@@ -6,7 +7,7 @@ from yhttp.extensions.pony import setup as setuporm, configure as configureorm
 app = Application()
 app.settings.merge('''
 db:
-  url: postgres://:@/yhttpponytest
+  url: postgres://postgres:postgres@localhost/foo
 ''')
 db = setuporm(app)
 
@@ -14,7 +15,15 @@ db = setuporm(app)
 def test_applicationcli():
     cliapp = CLIApplication('example', 'tests.test_cli:app.climain')
     #configureorm(app)
-    with Given(cliapp, 'db --help', working_directory='.'):
-        print(stderr)
+    with Given(cliapp, 'db --help'):
         assert status == 0
+        assert stderr == ''
 
+        when('db drop')
+        when('db create')
+        assert status == 0
+        assert stderr == ''
+
+        when('db drop')
+        assert status == 0
+        assert stderr == ''
