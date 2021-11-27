cfe6391a200c9c6a17ef3731d46db342ed7abafb
pylover <vahid.mardani@gmail.com>
2021-11-27 13:13:23 +0330

    Fight: github action yaml error

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,42 +1,42 @@
-# from bddcli import Given, Application as CLIApplication, status, stderr, \
-#     when, stdout
-# import easycli
-# from yhttp import Application
-# from yhttp.ext.pony import install
-#
-#
-# class Bar(easycli.SubCommand):
-#     __command__ = 'bar'
-#
-#     def __call__(self, args):
-#         print('bar')
-#
-#
-# app = Application()
-# app.settings.merge('''
-# db:
-#   url: postgres://postgres:postgres@localhost/foo
-# ''')
-# db = install(app, cliarguments=[Bar])
-#
-#
-# def test_applicationcli():
-#     cliapp = CLIApplication('example', 'tests.test_cli:app.climain')
-#     with Given(cliapp, 'db --help'):
-#         assert status == 0
-#         assert stderr == ''
-#
-#         when('db drop')
-#         when('db create')
-#         assert status == 0
-#         assert stderr == ''
-#
-#         when('db drop')
-#         assert status == 0
-#         assert stderr == ''
-#
-#         # Custom Command line interface
-#         when('db bar')
-#         assert status == 0
-#         assert stderr == ''
-#         assert stdout == 'bar\n'
+from bddcli import Given, Application as CLIApplication, status, stderr, \
+    when, stdout
+import easycli
+from yhttp import Application
+from yhttp.ext.pony import install
+
+
+class Bar(easycli.SubCommand):
+    __command__ = 'bar'
+
+    def __call__(self, args):
+        print('bar')
+
+
+app = Application()
+app.settings.merge('''
+db:
+  url: postgres://postgres:postgres@localhost/foo
+''')
+db = install(app, cliarguments=[Bar])
+
+
+def test_applicationcli():
+    cliapp = CLIApplication('example', 'tests.test_cli:app.climain')
+    with Given(cliapp, 'db --help'):
+        assert status == 0
+        assert stderr == ''
+
+        when('db drop')
+        when('db create')
+        assert status == 0
+        assert stderr == ''
+
+        when('db drop')
+        assert status == 0
+        assert stderr == ''
+
+        # Custom Command line interface
+        when('db bar')
+        assert status == 0
+        assert stderr == ''
+        assert stdout == 'bar\n'
